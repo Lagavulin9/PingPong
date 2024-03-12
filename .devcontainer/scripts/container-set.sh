85c1f203b60c8317a7c8e704267a108f84f33994
jinholee <jinholee@student.42seoul.kr>
2024-03-12 20:31:15 +0000

    symlink for commonapi.ini

--- .devcontainer/scripts/container-set.sh
+++ .devcontainer/scripts/container-set.sh
@@ -1,6 +1,15 @@
 echo "#######################################################"
 echo "### Checking container creation                     ###"
 echo "#######################################################"
+# Create remote user
 useradd vscode -m
 echo 'vscode:vscode' | chpasswd
 usermod -aG sudo vscode
+
+# Create a symbolic link for .ini
+if [ -f "conf/commonapi.ini" ]; then
+	ln -s "$(pwd)/conf/commonapi.ini" /etc/commonapi.ini
+    echo "Symbolic link for commonapi.ini created successfully."
+else
+    echo "commonapi.ini does not exist."
+fi
--- .devcontainer/scripts/onCreate.sh
+++ .devcontainer/scripts/onCreate.sh
@@ -1,4 +1,1 @@
 # Routines for onCreate
-
-# Create a symbolic link for .ini
-# ln -s /workspace/conf/commonapi.ini /etc/commonapi.ini
