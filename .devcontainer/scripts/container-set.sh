fd76880094d27c4d493238ff220dee4658f15917
jinholee <jinholee@student.42seoul.kr>
2024-03-15 07:34:21 +0900

    seperated commonapi

--- .devcontainer/scripts/container-set.sh
+++ .devcontainer/scripts/container-set.sh
@@ -3,11 +3,3 @@ useradd vscode -m
 echo 'vscode:vscode' | chpasswd
 usermod -aG sudo vscode
 chown vscode:vscode /workspace
-
-# Create a symbolic link for .ini
-if [ -f "conf/commonapi.ini" ]; then
-	ln -s "$(pwd)/conf/commonapi.ini" /etc/commonapi.ini
-    echo "Symbolic link for commonapi.ini created successfully."
-else
-    echo "commonapi.ini does not exist."
-fi
